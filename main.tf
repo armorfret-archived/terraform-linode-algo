97b779634335c25af5592f79c734db0928a67d6a
Les Aker <me@lesaker.org>
2018-10-12 11:36:23 -0400

    break up config block

--- main.tf
+++ main.tf
@@ -14,7 +14,25 @@ resource "linode_instance" "algo" {
   region = "${var.region}"
   type   = "${var.type}"
 
-  authorized_keys = "${var.ssh_keys}"
+  disk {
+    label           = "root"
+    size            = 10240
+    authorized_keys = "${var.ssh_keys}"
+    image           = "linode/ubuntu18.04"
+  }
+
+  config {
+    label  = "default"
+    kernel = "linode/grub2"
+
+    devices {
+      sda = {
+        disk_label = "root"
+      }
+    }
+  }
+
+  boot_config = "default"
 
   provisioner "remote-exec" {
     script = "${path.module}/assets/system_init.sh"
