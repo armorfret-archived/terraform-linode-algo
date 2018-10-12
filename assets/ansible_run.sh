7b3f15673a0af3be54725296cf53569ff1b7aec1
Les Aker <me@lesaker.org>
2018-10-12 16:57:45 -0400

    update ansible flags

--- assets/ansible_run.sh
+++ assets/ansible_run.sh
@@ -10,15 +10,14 @@ ip_addr="$(ip -4 addr show eth0 | awk '/inet/ {print $2}' | sed 's|/[0-9]*$||')"
 password="$(cat /dev/shm/algo_password)"
 shred /dev/shm/algo_password
 
-ansible-playbook \
-    main.yml \
-    -t 'local,vpn' \
-    --skip-tags '_null,encrypted,cloud,update-alternatives' \
-    -e "server_ip=localhost
-        server_user=root
-        IP_subject_alt_name=${ip_addr}
-        OnDemandEnabled_Cellular=Y
-        OnDemandEnabled_WIFI=Y
-        OnDemandEnabled_WIFI_EXCLUDE='_null'
-        Store_CAKEY=N
         p12_export_password='${password}'"
+
+ansible-playbook main.yml -e "
+    provider=local
+    ondemand_cellular=true
+    ondemand_wifi=true
+    local_dns=false
+    ssh_tunneling=true
+    windows=false
+    store_cakey=false
+    p12_password='${password}'"
