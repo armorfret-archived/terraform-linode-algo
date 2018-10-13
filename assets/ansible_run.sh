c5fb353c91cf6a6b9836cadba43d48afb7bb2659
Les Aker <me@lesaker.org>
2018-10-13 15:05:55 -0400

    handle shred and exclude

--- assets/ansible_run.sh
+++ assets/ansible_run.sh
@@ -8,12 +8,13 @@ source env/bin/activate
 
 ip_addr="$(ip -4 addr show eth0 | awk '/inet/ {print $2}' | sed 's|/[0-9]*$||')"
 password="$(cat /dev/shm/algo_password)"
-shred /dev/shm/algo_password
+shred -u /dev/shm/algo_password
 
 ansible-playbook main.yml -e "
     provider=local
     ondemand_cellular=true
     ondemand_wifi=true
+    ondemand_wifi_exclude=''
     local_dns=false
     ssh_tunneling=true
     windows=false
