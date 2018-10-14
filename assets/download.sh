a80e52efdaa6d163466e545e1c0cb2656504f421
Les Aker <me@lesaker.org>
2018-10-15 01:02:20 +0300

    make config dir

--- assets/download.sh
+++ assets/download.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 server_ip="$1"
 config_dir="configs/${server_ip}"
 
+mkdir -p "$config_dir"
 scp \
     -oStrictHostKeyChecking=no \
     "root@${server_ip}:/opt/algo/${config_dir}/*.mobileconfig" \
