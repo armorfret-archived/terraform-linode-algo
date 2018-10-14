489cb60b7f18bed78aa871cd307b770541364890
Les Aker <me@lesaker.org>
2018-10-15 00:46:01 +0300

    password handling is now automated

--- assets/deploy.sh
+++ assets/deploy.sh
@@ -7,8 +7,6 @@ export PS1=""
 source env/bin/activate
 
 ip_addr="$(ip -4 addr show eth0 | awk '/inet/ {print $2}' | sed 's|/[0-9]*$||')"
-password="$(cat /dev/shm/algo_password)"
-shred -u /dev/shm/algo_password
 
 ansible-playbook main.yml -e "
     provider=local
@@ -21,5 +19,4 @@ ansible-playbook main.yml -e "
     local_dns=false
     ssh_tunneling=true
     windows=false
-    store_cakey=false
-    p12_password='${password}'"
+    store_cakey=false"
