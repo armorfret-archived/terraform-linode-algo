ab884592ca729cee7b37db4e5d3e5c8d0e4e5096
Les Aker <me@lesaker.org>
2018-12-26 14:47:14 -0500

    capture wireguard configs

--- assets/download.sh
+++ assets/download.sh
@@ -4,12 +4,15 @@ set -euo pipefail
 
 server_ip="$1"
 server_name="$2"
-src_path="/opt/algo/configs/${server_ip}/*.mobileconfig"
+src_root_path="/opt/algo/configs/${server_ip}"
+src_objects="*.mobileconfig wireguard/*.png wireguard/*.conf"
 target_dir="configs/${server_name}"
 
 mkdir -p "$target_dir"
-scp \
-    -oStrictHostKeyChecking=no \
-    "root@${server_ip}:${src_path}" \
-    "${target_dir}/"
+for src_object in $src_objects ; do
+    scp \
+        -oStrictHostKeyChecking=no \
+        "root@${server_ip}:${src_root_path}/${src_object}" \
+        "${target_dir}/"
+done
 
