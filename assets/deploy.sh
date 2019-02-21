913b70537a734138b573b00852eb7001a1f2cc07
Les Aker <me@lesaker.org>
2019-02-21 17:29:43 +0000

    update script

--- assets/deploy.sh
+++ assets/deploy.sh
@@ -2,7 +2,25 @@
 
 set -euo pipefail
 
-cd /opt/algo
-export PS1=""
-source env/bin/activate
-ansible-playbook main.yml
+ALGO_DIR=/opt/algo
+if [[ ! -e "$ALGO_DIR" ]] ; then
+    git clone "${algo_repo}" "$ALGO_DIR"
+fi
+
+(
+    cd "$ALGO_DIR"
+
+    if [[ "$(git remote get-url origin)" != "${algo_repo}" ]] ; then
+        echo "Repo in image is not the expected repo"
+        exit 1
+    fi
+
+    git pull
+
+    [[ -e env ]] && rm -rf env
+    virtualenv env
+    export PS1=""
+    source env/bin/activate
+    pip install --force-reinstall --no-cache-dir -r requirements.txt
+    ansible-playbook main.yml
+)
