38ec9e22787fe8cafa93e055cb4ccced14108502
Les Aker <me@lesaker.org>
2019-04-02 16:48:48 +0100

    add users field

--- variables.tf
+++ variables.tf
@@ -6,6 +6,13 @@ variable "name" {
 variable "ssh_keys" {
   type        = "list"
   description = "SSH public keys used to log in as root to the server"
+  default     = []
+}
+
+variable "users" {
+  type        = "list"
+  description = "List of authorized users (Linode usernames, must have keys already assigned via Linode API)"
+  default     = []
 }
 
 variable "region" {
